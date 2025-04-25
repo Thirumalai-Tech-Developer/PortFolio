05a2d0f5b42ea44d786b664c5552bfb78f335cd5
Thirumalai-Tech-Developer <thirtechdeveloper@gmail.com>
2025-04-25 21:50:16 +0530

    add: basic Header bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-
       <Footer />
     </div>
   );
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../App.css'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center p-3'>
+      <div className='flex gap-20 justify-center bg-black bg-opacity-10 rounded-full w-[70%] p-3'>
+        <a>About</a>
+        <a>Projects</a>
+        <a>Contact</a>
+      </div>
     </div>
   )
 }
