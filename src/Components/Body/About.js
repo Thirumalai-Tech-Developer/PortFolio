bb24c5838887866276f0825903d436dd687a7eaf
Thirumalai-Tech-Developer <thirtechdeveloper@gmail.com>
2025-04-26 10:45:44 +0530

    feat: Basic About with responsiveness

--- src/Components/Body/About.js
+++ src/Components/Body/About.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Data from '../Data/details.json'
 
 const About = () => {
   return (
-    <div>
+    <div className='flex justify-evenly p-2'>
+      <div className='flex justify-center p-10 bg-slate-800 bg-opacity-30 hover:bg-opacity-45 duration-700 rounded-2xl sm:w-[90%] md:w-[90%] lg:w-[50%] text-base sm:text-md md:text-xl lg:text-lg xl:text-xl'>
+        {
+          Data.map((i) => {
+            return (
+              <div key={i.key}>
+                <h1 className="text-[50px] py-4">{i.name}</h1>
+                <br></br>
+                <p style={{textIndent: '2rem'}}>{i.objective}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className='hidden lg:flex w-[50%] justify-center'>
+        <h1>
+          hello
+        </h1>
+      </div>
     </div>
   )
 }
--- src/Components/header.js
+++ src/Components/header.js
@@ -5,7 +5,7 @@ import '../App.css'
 const Header = () => {
   return (
     <div className='flex justify-center p-3'>
-      <div className='flex gap-20 justify-center bg-black bg-opacity-10 rounded-full w-[70%] p-3 lg:w-[50%]'>
+      <div className='flex gap-20 justify-center bg-black bg-opacity-10 rounded-full p-3 md:w-[90%] lg:w-[50%] xl:w-[40%]'>
         <Link to="/about">About</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/contact">Contact</Link>
